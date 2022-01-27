c25051635a5e058f394ea7d4f2d2071ba0cd52c7
anhbch <90117476+anhbch@users.noreply.github.com>
2022-01-26 18:35:06 -0800

    test file 1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,24 @@
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFile() throws IOException {
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList <String> expectedOutPut = new ArrayList<String>();
+        expectedOutPut.add("https://something.com");
+        expectedOutPut.add("some-page.html");
+        assertEquals(expectedOutPut, links);
     }
 }
