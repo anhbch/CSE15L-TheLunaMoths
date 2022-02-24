058a555295c69acee83bfb70d7e41484b2c8f484
anhbch <90117476+anhbch@users.noreply.github.com>
2022-02-24 12:21:00 -0800

    test for snippet 2+3

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -103,7 +103,20 @@ public class MarkdownParseTest {
         String contents= Files.readString(Path.of("./snippet1.md"));
         List<String> expect = List.of("google.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
-  
+    }
+
+    @Test
+    public void TestSnippet2() throws IOException {
+        String contents= Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com, b.com, a.com(()), example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void TestSnippet3() throws IOException {
+        String contents= Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://www.twitter.com, https://ucsd-cse15l-w22.github.io/, github.com, https://cse.ucsd.edu/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 }
 
